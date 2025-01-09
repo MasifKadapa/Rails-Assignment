59079e9cf26717f2f4c18f2fc514dc097b5390b4
MasifKadapa <ioioi200485@gmail.com>
2025-01-09 11:11:23 +0530

    Delete task3.py

--- task3.py
+++
@@ -1,9 +0,0 @@
-def multiply_and_add(x, y):
-    result = x * y
-    total = result + y
-    return total
-
-def add_and_calculate(x, y):
-    sum_value = x + y
-    product_plus_sum = multiply_and_add(x, y)
-    return sum_value, product_plus_sum
