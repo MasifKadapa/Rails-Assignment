5661828e7e105cb69002da22eb0992c7082e100e
MasifKadapa <ioioi200485@gmail.com>
2025-01-09 11:11:13 +0530

    Delete process.py

--- process.py
+++
@@ -1,7 +0,0 @@
-def process_status(status_dictionary):
-    if status_dictionary['status'] == 'active':
-        print('Active State')
-    elif status_dictionary['status'] == 'inactive':
-        print('Inactive State')
-    else:
-        print('Unknown State')
